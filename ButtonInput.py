085145206c322f40f5a8bb75d3297da7493f1682
Fabian Canas <fcanas@gmail.com>
2021-05-14 00:47:19 -0400

    Document button handling

--- ButtonInput.py
+++ ButtonInput.py
@@ -3,6 +3,56 @@ import digitalio
 import time
 from enum import Enum
 
+# This file handles input on two "button" states as read from digital IO pins
+# and detects three possible "gestures":
+#
+# 1. First Button Pressed
+# 2. Second Button Pressed
+# 3. Both Buttons Pressed Together
+#
+# Handling cases 1 and 2 are trivial with a fast enough polling interval.
+# Case 3 proves problematic because the two buttons are unlikely to be
+# pressed within the same polling interval when it's fast, and the
+# interactivity experience suffers for all cases when input polling is
+# slow. So we have to carefully manage expectations of button interaction.
+#
+# The first thing to note is that the action is committed / detected / 
+# accepted when the button(s) for the gesture become un-pressed. The 
+# second is that the first and second button may become depressed on two
+# separate poll passes, but within some interval τ, they should be treated
+# as if they were pressed together.
+#
+# While both buttons are pressed, the two may become unpressed in separate
+# polling passes. Within a time interval τ, this should be treated as if
+# they were released together. 
+#
+# The opportunity exists to encode more "gestures" taking advantage of the
+# aforementioned timeouts and combinations of presses and holds. This
+# system instead allows for any state with any button down to be carefully
+# converted into any other gesture. The only thing you can't do is cancel
+# all possible actions. In that way, the interface is as as forgiving of
+# mistakes in input as I could make it.
+#
+# These requirements are modeled as a state machine:
+#
+#  -==[ Meaningful States ]==-
+#
+#   ω  →  empty state. No buttons pressed
+#  [A] →  Accepted A state: "first button pressed"
+#  [B] →  Accepted B state: "second button pressed"
+#  [C] →  Accepted C state: "first button and second buttons pressed together"
+#
+#     Other states can be described, but don't necessarily have short,
+#     intuitive labels.
+#
+#  -==[    Transitions    ]==-
+#
+#  +α  →  first button pressed
+#  +β  →  second button pressed
+#   τ  →  timeout elapses - Check `ca` and `cb` states: time elapse is 
+#		  evaluated before checking for +/-:α/β. The order isn't
+#		  critical but should be consistent.
+#
 #               ╔═══╗
 #    ┌──┐   -α  ║ A ║
 #   τ│  │   ┌──▶╚═══╝    τ     ┌───┐
@@ -12,8 +62,8 @@ from enum import Enum
 #       └───┘╲                 │   │       -α│
 #       ▲     ╲               -β   +β        │
 #       │+α    ╲─────+β─────╲  │   │         ▼
-#       │                    ╲ │   ▼         ╔═══╗
-#  ┌──┏━━━┓                   ▼┌───┐───-αβ──▶║ C ║
+#       │                    ╲ │   ▼   -αβ   ╔═══╗
+#  ┌──┏━━━┓                   ▼┌───┐────────▶║ C ║
 # τ│  ┃ ω ┃─────+αβ───────────▶│ c │───┐     ╚═══╝
 #  └─▶┗━━━┛                   ▲└───┘◀──┘τ    ▲
 #       │                    ╱ │   ▲         │
@@ -120,9 +170,8 @@ class ButtonInput:
 			elif (time.time() - self.captureTime) > self.tau:
 				self.buttonState = ButtonState.BETA
 			elif not b:
-				self.buttonState = ButtonState.C_ACCEPT
-				
-				
+				self.buttonState = ButtonState.C_ACCEPT	
+		
 		def c_accept(a,b):
 			self.captureTime = None
 			self.abAction()
